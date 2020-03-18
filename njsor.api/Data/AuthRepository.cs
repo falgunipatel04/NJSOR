0de379aab5e6b9d689016a41c84fd4c61aeba7c1
Falguni Patel <Falguni.Patel@dhs.nj.gov>
2020-03-18 15:51:37 -0400

    authrepository modified

--- njsor.api/Data/AuthRepository.cs
+++ njsor.api/Data/AuthRepository.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using njsor.api.Model;
+
 
 namespace njsor.api.Data
 {
@@ -11,19 +14,63 @@ namespace njsor.api.Data
             _context = context;
 
         }
-        public Task<User> Login(string username, string Password)
+        public async Task<User> Login(string username, string Password)
         {
-            throw new System.NotImplementedException();
+           var user= await _context.Users.FirstOrDefaultAsync(x=>x.Username== username);
+           if (user == null)
+           return null;
+           if(!VerifyPasswordHash(Password,user.PasswordHash,user.PasswordSalt))
+           return null;
+
+           return user;
         }
 
-        public Task<User> Register(User User, string Password)
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
         {
-            throw new System.NotImplementedException();
+             using ( var hvac= new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+               
+                var computedHash=hvac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                for(int i=0; i<computedHash.Length; i++)
+                {
+                    if(computedHash[i] != passwordHash[i])
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
         }
 
-        public Task<bool> UserExist(string username)
+        public async Task<User> Register(User user, string Password)
         {
-            throw new System.NotImplementedException();
+           byte[] passwordHash,passwordSalt;
+           CreatePasswordHash(Password,out passwordHash,out passwordSalt);
+           user.PasswordHash=passwordHash;
+           user.PasswordSalt=passwordSalt;
+           await _context.Users.AddAsync(user);
+           await _context.SaveChangesAsync();
+           return user;
         }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using ( var hvac= new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hvac.Key;
+                passwordHash=hvac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+           
+        }
+
+        public async Task<bool> UserExist(string username)
+        {
+            if (await _context.Users.AnyAsync(x=>x.Username == username))
+            return true;
+
+            return false;
+        }
+
+
     }
 }
